2d97a33e0d4332a093b027701cf894b38e9e5479
Téo HUBERT <teo.hubert@etudiant.univ-rennes.fr>
2025-12-02 12:05:09 +0100

    Add comment

--- serveur/models/indicator.py
+++ serveur/models/indicator.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
 
+## CETTE CLASS N'EST PAS ENCORE UTILISEE DANS LE CODE ##
+
 class Indicator(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     host_id: Optional[int] = Field(default=None, foreign_key="host.id")
