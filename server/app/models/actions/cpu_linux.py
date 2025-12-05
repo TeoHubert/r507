b9abbb0edff2ac9db666f77c412feaddc9e914bc
TeoHubert <teohubert00@gmail.com>
2025-12-05 14:39:16 +0100

    Mise à jour de la fonction de calcul de l'utilisation du CPU Linux

--- server/app/models/actions/cpu_linux.py
+++ server/app/models/actions/cpu_linux.py
@@ -2,7 +2,7 @@ from models.host import Host
 
 def run(host: Host) -> str:
     try:
-        value = float(host.execute_ssh_command("grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}'"))
+        value = float(host.execute_ssh_command('read -r cpu user nice system idle iowait irq softirq steal guest < /proc/stat; sleep 1; read -r cpu_n user_n nice_n system_n idle_n iowait_n irq_n softirq_n steal_n guest_n < /proc/stat; PREV_TOTAL=$((user+nice+system+idle+iowait+irq+softirq+steal)); CURRENT_TOTAL=$((user_n+nice_n+system_n+idle_n+iowait_n+irq_n+softirq_n+steal_n)); PREV_IDLE=$idle; CURRENT_IDLE=$idle_n; DIFF_TOTAL=$((CURRENT_TOTAL-PREV_TOTAL)); DIFF_IDLE=$((CURRENT_IDLE-PREV_IDLE)); CPU_USAGE=$(( (DIFF_TOTAL-DIFF_IDLE) * 100 / DIFF_TOTAL )); echo "$CPU_USAGE"'))
         return value
     except Exception as e:
         return f"Error: {e}"
