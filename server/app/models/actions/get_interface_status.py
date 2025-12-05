9d3f044058da8ad5feb7a7dda6ee9ba2f42e6560
TeoHubert <teohubert00@gmail.com>
2025-12-05 15:23:56 +0100

    Ajout de la fonction run pour récupérer le statut d'une interface via SSH

---
+++ server/app/models/actions/get_interface_status.py
@@ -0,0 +1,20 @@
+from models.host import Host
+import json
+
+def run(host: Host, parametre: str = None) -> str:
+    try:
+        result = host.execute_ssh_command("vtysh -c 'show interface brief json'")
+        print(result)
+        result = result.replace("'", '"')
+        data = json.loads(result)
+        status = data[parametre['interface']]['status']
+        if status == "up": status = 2
+        elif status == "down": status = 1
+        else: status = 0
+        return status
+    except Exception as e:
+        print(f"Erreur lors de la récupération du statut de l'interface: {e}")
+        return 0
+
+if __name__ == "__main__":
+    print(run())
